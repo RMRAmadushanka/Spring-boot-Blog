31a2a35448b24b02ee378205f6fe48846378bde0
RMRAmadushanka <rmramadushanka@gmail.com>
2024-08-05 13:57:21 +0530

    Added bean validation

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/CommentDto.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -1,6 +1,9 @@
 package com.springboot.blog.payload;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,7 +11,12 @@ import lombok.Setter;
 @Setter
 public class CommentDto {
     private long id;
+    @NotEmpty(message = "Name should not be null or empty")
     private String name;
+    @NotEmpty(message = "email should not be null or empty")
+    @Email
     private String email;
+    @NotEmpty
+    @Size(min = 10, message = "Comment body must be minimum 10 character")
     private String body;
 }
