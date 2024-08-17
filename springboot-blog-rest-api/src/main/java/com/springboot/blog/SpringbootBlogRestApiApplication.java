b5183323049479dcaa940b443117437fb8d67526
RMRAmadushanka <rmramadushanka@gmail.com>
2024-08-17 22:53:14 +0530

    Added swagger api info

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/SpringbootBlogRestApiApplication.java
@@ -1,11 +1,24 @@
 package com.springboot.blog;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info=@Info(
+				title = "Spring Boot Blog APP APIs",
+				description = "Spring boot Blog APP API documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Rolan"
+				)
+		)
+)
 public class SpringbootBlogRestApiApplication {
 	@Bean
 	public ModelMapper modelMapper (){
